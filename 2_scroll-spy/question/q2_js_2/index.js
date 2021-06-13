254374845a33bd5f747780cc66babee8a8668e7c
jjm2317 <wlaks2317@gmail.com>
2021-06-13 15:25:28 +0900

    feat: (ScrollSpy) Maintain feature when resizing

--- 2_scroll-spy/question/q2_js_2/index.js
+++ 2_scroll-spy/question/q2_js_2/index.js
@@ -4,19 +4,32 @@ const navElem = document.querySelector("#nav");
 const navItems = Array.from(navElem.children);
 const contentsElem = document.querySelector("#contents");
 const contentItems = Array.from(contentsElem.children);
-const getOffsetTops = null; // do something
+const getOffsetTops = () =>
+  contentItems.map(({ offsetTop, clientHeight }) => [
+    offsetTop - clientHeight / 2,
+    offsetTop + clientHeight / 2,
+  ]);
 
-window.addEventListener("scroll", e => {
-  // fix here
+window.addEventListener("scroll", (e) => {
+  const { scrollTop } = e.target.scrollingElement;
+  const navIndex = getOffsetTops().findIndex(
+    ([from, to]) => scrollTop >= from && scrollTop < to
+  );
+  if (!navItems[navIndex].classList.contains("on")) {
+    navItems.forEach(
+      (item, i) => i !== navIndex && item.classList.remove("on")
+    );
+    navItems[navIndex].classList.add("on");
+  }
 });
 
-navElem.addEventListener("click", e => {
+navElem.addEventListener("click", (e) => {
   const targetElem = e.target;
   if (targetElem.tagName === "BUTTON") {
     const targetIndex = navItems.indexOf(targetElem.parentElement);
     contentItems[targetIndex].scrollIntoView({
       block: "start",
-      behavior: "smooth"
+      behavior: "smooth",
     });
   }
 });
