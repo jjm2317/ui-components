fe9534f6ab6616865f20705e49ad2d549c762669
jjm2317 <wlaks2317@gmail.com>
2021-06-16 21:09:27 +0900

    feat:(ScrollSpy) Apply IntersectionObserver

--- 2_scroll-spy/question/q5_js_Intersection_Observer/index.js
+++ 2_scroll-spy/question/q5_js_Intersection_Observer/index.js
@@ -6,24 +6,33 @@ const contentsElem = document.querySelector("#contents");
 const contentItems = Array.from(contentsElem.children);
 
 const scrollSpyObserver = new IntersectionObserver(
-  entries => {
-    // do something
+  (entries) => {
+    const outItem = entries.find((item) => !item.isIntersecting)?.target;
+    const inItem = entries.find((item) => item.isIntersecting)?.target;
+    if (outItem) {
+      const outIndex = contentItems.findIndex((item) => item === outItem);
+      navItems[outIndex].classList.remove("on");
+    }
+    if (inItem) {
+      const inIndex = contentItems.findIndex((item) => item === inItem);
+      navItems[inIndex].classList.add("on");
+    }
   },
   {
     root: null,
     rootMargin: "0px",
-    threshold: 0.5
+    threshold: 0.5,
   }
 );
-contentItems.forEach(item => scrollSpyObserver.observe(item));
+contentItems.forEach((item) => scrollSpyObserver.observe(item));
 
-navElem.addEventListener("click", e => {
+navElem.addEventListener("click", (e) => {
   const targetElem = e.target;
   if (targetElem.tagName === "BUTTON") {
     const targetIndex = navItems.indexOf(targetElem.parentElement);
     contentItems[targetIndex].scrollIntoView({
       block: "start",
-      behavior: "smooth"
+      behavior: "smooth",
     });
   }
 });
