af85a2e4a9daacfbf196822691fb7a7e3da26ecf
jjm2317 <wlaks2317@gmail.com>
2021-07-06 22:36:29 +0900

    fix: theme property light

--- 4_dark-mode/question/3.react/src/style/theme.js
+++ 4_dark-mode/question/3.react/src/style/theme.js
@@ -4,8 +4,15 @@ const dark = {
   switchLeft: 52,
   buttonTextBgc: "#fc3164",
 };
+const light = {
+  bgc: "#fff",
+  switchLeft: 2,
+  color: "#000",
+  buttonTextBgc: "#3dbf87",
+};
 const theme = {
   dark,
+  light,
 };
 
 export default theme;
